e1133eb394c654f661f0a16abd9a144bde37e3ef
Dagg Stompler <daggs@gmx.com>
2021-04-15 10:43:39 +0300

    add license
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- create_installer.sh
+++ create_installer.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# This file is part of the simpler pkg installer creation (https://github.com/daggs1/simple_pkg_installer).
+# Copyright (c) 2021 Dagg (daggs@gmx.com).
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# @version: 1.0
+
 entries_to_pack=()
 declare -A additiona_payload
 post_install_exec=
